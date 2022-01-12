17c5e0d511daae923411581f282c6bf88b45774b
Rita-Gujar <ritagujar14@gmail.com>
2022-01-12 10:03:00 +0530

    Fixed Scroll issue

--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -5,16 +5,17 @@ export const LoginContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  margin-top: 2rem;
-  overflow: hidden;
+  /* max-width: 100vh; */
+  /* margin-top: 2rem; */
+  justify-content: center;
+  /* overflow: hidden; */
 `;
 export const StyleText = styled.h1`
-  margin-bottom: 10px;
-  /* color: rgb(7, 8, 28); */
   color: #242455;
   font-style: italic;
 `;
 export const Text = styled.p`
+  margin-top: 8px;
   margin-bottom: 20px;
   color: #617287;
   font-size: 1.2rem;
@@ -30,7 +31,7 @@ export const Card = styled.div`
   background: white;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1.3rem 1.3rem;
-  margin-bottom: 20px;
+  /* margin-bottom: 20px; */
   color: #07081c;
   input {
     height: 42px;
@@ -58,7 +59,7 @@ export const Button = styled.button`
   margin: 0 auto;
   margin-top: 20px;
   width: 145px;
-  height: 42px;
+  height: 38px;
   border: none;
   color: #ffffff;
   background-image: linear-gradient(80deg, #ac3678, #d8314f, #fa8246);
